f5ee3314c4e0055624406b0e4cd320697aa2b5eb
eloudsa <said@noratek.net>
2018-11-14 12:28:33 +0100

    Show/Hide New Asset action depending the sum of tokens owned by the account

--- src/components/Assets/Assets.js
+++ src/components/Assets/Assets.js
@@ -554,19 +554,22 @@ class Assets extends Component {
                     <Row>
                         <Col xs={12} lg={12}>
                             <div className={"addItem"}>
-                                <Button className={"add-button margin-button"} variant="contained" color="primary"
+                                {this.tokens >= this.serviceFee &&
+                                    <Button className={"add-button margin-button"} variant="contained" color="primary"
+                                            onClick={() => {
+                                                this.handleNewAsset();
+                                            }}>
+                                        New Asset
+                                    </Button>
+                                }
+
+                                <Button className={"add-button"} variant="contained" color="primary"
                                         onClick={() => {
                                             this.handleBuyTokens();
                                         }}>
                                     Buy Tokens
                                 </Button>
 
-                                <Button className={"add-button"} variant="contained" color="primary"
-                                        onClick={() => {
-                                            this.handleNewAsset();
-                                        }}>
-                                    New Asset
-                                </Button>
                             </div>
                         </Col>
                     </Row>
